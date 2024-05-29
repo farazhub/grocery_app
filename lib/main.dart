98f8ea3eb37152010765d84a93c3143e16c5a93b
Faraz Haider Kazmi <haiderfaraz81@gmail.com>
2024-05-29 11:39:27 +0500

    changes from lenovo  28th may

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,18 @@
 import 'package:flutter/material.dart';
+import 'splash_screen.dart'; // Make sure to import your splash screen file
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(useMaterial3: true, colorScheme: ColorScheme.light()),
-        home: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Container(
-                height: MediaQuery.of(context).size.height*0.24,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(30),
-                  ),
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 2,
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Oooops!!!',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                      ),
-                    Text('No internet connection found, Please check your internet connection',
-                      style: TextStyle(
-
-                      ),
-                      ),
-
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ));
+      theme: ThemeData(
+        fontFamily: 'Poppins-Medium.ttf',
+      ),
+      home: SplashScreen(), // Set the splash screen as the initial route
+    );
   }
 }
