9a22b161cc61bab336070dc090d89f6e2ebbf6f4
Faraz Haider Kazmi <haiderfaraz81@gmail.com>
2024-05-29 00:30:19 -0700

    fetched files from lenovo branch to sabacpc banch

---
+++ lib/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage("images/splash.png"),
+          ),
+          Text(
+            'Grocery App',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
